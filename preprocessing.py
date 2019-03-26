be39b16003a080c3d582f0f070bb8604c23b88ae
your name qqsuhao <qq7835144@outlook.com>
2019-03-26 19:15:11 +0800

    load data with saving as json

---
+++ preprocessing.py
@@ -0,0 +1,16 @@
+# -*- coding:utf8 -*-
+# @TIME     : 2019/3/26 17:06
+# @Author   : SuHao
+# @File     : preprocessing.py
+
+
+def denoise_of_wave():
+    pass
+
+
+def silence():
+    pass
+
+
+def frame():
+    pass
